cc9b1d6cd8d5320cbda3b4f7b32dbb328e368149
kel-man <kyyafuso@gmail.com>
2021-07-30 15:51:28 -0700

    All done besides research

--- code_challenges.rb
+++ code_challenges.rb
@@ -8,10 +8,26 @@ letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+# Looping through the array, I check if the current string includes the given substring
+# if it does, I add it to my output array
 
+def array_selector words, substring
+  output = []
+  words.each do |word|
+    if word.include? substring
+      output.push word
+    end
+  end
+  output #It was returning the input before I added this line. I guess line 16 is what it considers to be the implicit return
+end
 
+p array_selector beverages_array, 'o'
+p array_selector beverages_array, 't'
 
-# -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+# Cool beans
+
+# -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided.
+# HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
 album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -20,15 +36,92 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# As delete_if does not work on strings, I need to split my string first
+# I'll create a dictionary of vowels in an array so I can check if the current letter is a vowel as I iterate over my array
+# Then I return a joined string from the array
 
+def remove_vowels string
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  stringArray = string.split('')
+  stringArray.delete_if{|letter| vowels.include? letter.downcase}
+  stringArray.join('')
+end
 
-# --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a get_info method that returns a sentence with all the data from the bike object.
+p remove_vowels album1
+p remove_vowels album2
+p remove_vowels album3
+
+# --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2.
+#                         The current_speed should start at 0. Create a get_info method that returns a sentence with all the data from the bike object.
+
+class Bike
+  attr_accessor :wheels, :current_speed, :model
+
+  def initialize model
+    @wheels = 2
+    @current_speed = 0
+    @model = model
+  end
+
+  def get_info
+    p "The #{model} bike has #{wheels} wheels and is moving at #{current_speed} mph"
+  end
+end
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+trek = Bike.new('Trek')
+trek.get_info
 
 
-
-# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
+# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed.
+#                        The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster 10 => 10
 # Expected output example: my_bike.brake 15 => 0
+#
+# I'm just going to leave the first class up there as the code below will override it
+
+class Bike
+  attr_accessor :wheels, :current_speed, :model
+
+  def initialize model
+    @wheels = 2
+    @current_speed = 0
+    @model = model
+  end
+
+  def pedal_faster
+    if @current_speed == 40
+      p 'You are going as fast as you are able'
+    end
+    @current_speed += 10 unless @current_speed >= 40
+  end
+
+  def brake
+    if @current_speed >= 15
+      @current_speed -= 15
+    else
+      @current_speed = 0
+      p 'You have stopped'
+    end
+  end
+
+  def get_info
+    p "The #{model} bike has #{wheels} wheels and is moving at #{current_speed} mph"
+  end
+end
+
+raleigh = Bike.new('Raleigh')
+raleigh.get_info
+raleigh.pedal_faster
+raleigh.get_info
+raleigh.brake
+raleigh.get_info
+5.times do
+  raleigh.pedal_faster
+  raleigh.get_info
+end
+4.times do
+  raleigh.brake
+  raleigh.get_info
+end
--- interview-questions.md
+++ interview-questions.md
@@ -3,11 +3,14 @@ Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
 
-Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.  
+Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
 1. What is an instance variable in Ruby? How is it different from other variables?
 
   Your answer:
+  Instance variables are referenced with the @ character and are the equivalent of this.variable in a Javascript class. It must be included in the
+  `attr_accessor` to be accessed, and it can be given an initial value in the initialize. Instance variables belong to an object. Other variables are typically
+  defined and used in a method or function, while instance variables belong to specific instances of a class(or object).
 
   Researched answer:
 
@@ -16,6 +19,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 2. What is a block in Ruby?
 
   Your answer:
+  In Ruby, blocks are essentially loops. Whenever iteration occurs, the ensuing code is a block. There are many more ways to implement iteration in Ruby than in
+  Javascript, and they are quite versatile.
 
   Researched answer:
 
@@ -24,6 +29,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 3. Ruby has an implicit return. What does that mean?
 
   Your answer:
+  It is not necessary to use the `return` keyword in Ruby. The last line of code that executes will return its output if the `return` keyword is omitted.
 
   Researched answer:
 
@@ -32,6 +38,9 @@ Then, research the question to expand on your answer. Even if you feel you have 
 4. What is object-oriented programming? How is it different than functional programming?
 
   Your answer:
+  In object oriented programming(hereon referred to as OOP), everything is an object. If a method exists, it is defined within an object. If a value exists, it belongs
+  to an object. In Ruby this is made exceedingly straightforward, as it is already object oriented as a language. Every single thing in Ruby is an object. This is
+  so extensive that if we wanted to, we could create a class that inherits from String and change the behavior of its methods.
 
   Researched answer:
 
@@ -40,6 +49,9 @@ Then, research the question to expand on your answer. Even if you feel you have 
 5. What is the difference between a class and an object?
 
   Your answer:
+  A class is a blueprint. It in itself just describes behavior. You cannot call a method that belongs to a class. It doesn't have variable values, just describes them.
+  An object is the realization of the behavior described in a class. You use a class to create an object, and the object can then be used to access its methods and
+  values.
 
   Researched answer:
 
@@ -48,6 +60,16 @@ Then, research the question to expand on your answer. Even if you feel you have 
 6. STRETCH: What is `attr_accessor`?
 
   Your answer:
+  `attr_accessor` is a built in method in Ruby classes that creates the basic behavior methods for the instance variables of that class.
+  A couple days ago I paired with my older brother Liam and refactored our rock-paper-scissors game to be OOP. He explained it thusly:
+      ` # attr_accessor is shorthand for defining these methods
+        # def num_players
+        #   self.num_players
+        # end
+        # def num_players= value
+        #   self.num_players value
+        # end
+        attr_accessor :players`
 
   Researched answer:
 
@@ -56,7 +78,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 ## Looking Ahead: Terms for Next Week
 
 1. PostgreSQL:
- 
+
 2. Ruby on Rails:
 
 3. CRUD:
